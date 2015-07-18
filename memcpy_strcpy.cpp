d9c30c2d39980405c1cbd5e7cdfdaea419f32dc6
C5Compound <C5Compound@hotmail.com>
2015-07-18 22:16:03 +0800

    实现memset和strlen函数

--- memcpy_strcpy.cpp
+++ memcpy_strcpy.cpp
@@ -1,29 +1,63 @@
 /**
  *  memcpy和strcpy的实现
+ *  记得判断指针的有效性
  */
 
 #include <cassert>
 #include <cstdlib>
 #include <cstdio>
-#include <iostream>
 using namespace std;
 
-char* strcpy(char *Dest, const char *Src)
+char* strcpy(char *dest, const char *src)
 {
-    assert(Dest != NULL && Src != NULL);
-    char *cp = Dest;
-    while ((*cp++ = *Src++) != '\0');
-    return Dest;
+    assert(dest != NULL && src != NULL);
+    char *cp = dest;
+    while ((*cp++ = *src++) != '\0');
+    return dest;
 }
 
-void memcpy(void *Dest, void *Src, int length)
+void* memcpy(void *dest, const void *src, size_t length)
 {
-
+    assert(dest != NULL && src != NULL);
+    // 不允许对void*指针进行自加和加法运算
+    char *_dest = (char*)dest;
+    const char *_src = (const char*)src;
+    // 从前往后拷贝
+    if (_dest <= _src) {
+        while (length-- > 0) {
+            *_dest++ = *_src++;
+        }
+    } else {
+        // 从后往前拷贝
+        while (length-- > 0) {
+            *(_dest + length) = *(_src + length);
+        }
+    }
+    return dest;
 }
 
-int main()
+void* memset(void *dest, char c, size_t size)
 {
-    char c = 255;
-    printf("%d\n", c);
-    return 0;
+    assert(dest != NULL);
+    char *_dest = (char*)dest;
+    while (size-- > 0) {
+        *_dest = c;
+    }
+    return dest;
+} 
+
+size_t strlen(const char *src)
+{
+    assert(src != NULL);
+    return *src == '\0' ? 0 : 1 + strlen(src + 1);
+}
+
+size_t strlen(const char *src)
+{
+    assert(src != NULL);
+    size_t len = 0;
+    while (*src++ != '\0') {
+        len += 1;
+    }
+    return len;
 }
