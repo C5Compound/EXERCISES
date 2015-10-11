9944765a389323275df3833f9ffc5bae7cd480d1
C5Compound <C5Compound@hotmail.com>
2015-10-11 21:46:03 +0800

    knapsack

--- 编程之美/背包问题及其变体.cpp
+++ 编程之美/背包问题及其变体.cpp
@@ -3,31 +3,45 @@
 using namespace std;
 
 // 0-1背包
-int knapsack_of_01(vector<pair<int, int>> &jewels, int cap) {
-    int dp[cap + 1];
-    for (int i = 1; i <= cap; ++i) {
-        for (int j = 0; j < jewels.size(); ++j) {
-            if (jewels[j].first < i) {
-                dp[i] = max(dp[i], dp[i - jewels[j].first] + jewels.second);
+int knapsack_of_01(vector<pair<int, int>> &jewels, int cap)
+{
+    vector<int> dp(cap + 1, 0);
+    for (int i = 1; i <= jewels.size(); ++i) {
+        for (int j = cap; j >= 1; --j) {
+            if (jewels[i - 1].first <= j) {
+                dp[j] = max(dp[j], dp[j - jewels[i - 1].first] + jewels[i - 1].second);
             }
         }
     }
     return dp[cap];
 }
 
-// 多重背包
-int knapsack_multi(vector<pair<int, int>> &jewels, int cap) {
-    
-    return 0;
-}
-
 // 完全背包
-int knapsack_complete(vector<pair<int, int>> &jewels, int cap) {
-
-    return 0;
+int knapscak_complete(vector<pair<int, int>> &jewels, int cap)
+{
+    vector<int> dp(cap + 1, 0);
+    for (int i = 1; i <= jewels.size(); ++i) {
+        for (int j = 1; j <= cap; ++j) {
+            if (jewels[i - 1].first <= j) {
+                dp[j] = max(dp[j], dp[j - jewels[i - 1].first] + jewels[i - 1].second);
+            }
+        }
+    }
+    return dp[cap];
 }
 
-int main() {
-    
-    return 0;
+// 二进制优化的多重背包，可以单独写，也可以先转化为01背包，用上面的函数求解
+int knapsack_multi(vector<pair<int, int>> &jewels, vector<int> &nums, int cap)
+{
+    vector<int> dp(cap + 1, 0);
+    for (int i = 1; i <= jewels.size(); ++i) {
+        for (int k = 1; k <= nums[i - 1]; k <<= 1) {
+            for (int j = cap; j >= 1; --j) {
+                if (jewels[i - 1].first * k <= j) {
+                    dp[j] = max(dp[j], dp[j - jewels[i - 1].first * k] + jewels[i - 1].second * k);
+                }
+            }
+        }
+    }
+    return dp[cap];
 }
